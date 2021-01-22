3ed90364555f9bd41e192d4cba6789f65f9160a0
Rahul Shial <rahul.shial@gmail.com>
2021-01-21 18:07:47 -0700

    Compare stats updates

--- src/Profile/GunCard.js
+++ src/Profile/GunCard.js
@@ -4,19 +4,21 @@ import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
 import ButtonBase from '@material-ui/core/ButtonBase';
+import Button from '@material-ui/core/Button';
 
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
+    backgroundColor: 'teal',
   },
   paper: {
     padding: theme.spacing(2),
     margin: 'auto',
-    maxWidth: 500,
+    maxWidth: '100%',
   },
   image: {
-    width: 128,
-    height: 128,
+    width: 140,
+    height: 140,
   },
   img: {
     margin: 'auto',
@@ -26,42 +28,100 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-export default function ComplexGrid() {
+export default function ComplexGrid(props) {
   const classes = useStyles();
+  const { gunImage, gunName} = props;
 
   return (
     <div className={classes.root}>
       <Paper className={classes.paper}>
         <Grid container spacing={2}>
-          <Grid item>
+          <Grid item direction column spacing={1} style={{maxWidth: '16%'}}>
             <ButtonBase className={classes.image}>
-              <img className={classes.img} alt="complex" src="/static/images/grid/complex.jpg" />
+              <img className={classes.img} alt="complex" src={gunImage}/>
             </ButtonBase>
+            <Paper className={classes.paper} gutterBottom style={{ display: 'flex', justifyContent: 'center', fontSize:15 }} variant="subtitle1">{gunName}</Paper>
           </Grid>
-          <Grid item xs={12} lg container>
-            <Grid item xs container direction="column" spacing={2}>
-              <Grid item xs container direction="row">
-              <Grid item xs>
-                <Paper gutterBottom variant="subtitle1">
-                  Standard license
-                </Paper>
-                <Paper variant="body2" gutterBottom>
-                  Full resolution 1920x1080 • JPEG
-                </Paper>
-                <Paper variant="body2" color="textSecondary">
-                  ID: 1030114
-                </Paper>
+          <Grid item xs sm container style={{maxWidth: '50%'}}>
+            <Grid item xs container direction='column' spacing={2}>
+              <Grid item xs container direction='row' spacing={2}>
+                <Grid xs={2}>
+                  <Paper className={classes.paper}>{gunName}</Paper>
+                </Grid>
+                <Grid xs={2}>
+                  <Paper className={classes.paper}>{gunName}</Paper>
+                </Grid>
+                <Grid xs={2}>
+                  <Paper className={classes.paper}>{gunName}</Paper>
+                </Grid>
+                <Grid xs={2}>
+                  <Paper className={classes.paper}>{gunName}</Paper>
+                </Grid>
+                <Grid xs={2}>
+                  <Paper className={classes.paper}>{gunName}</Paper>
+                </Grid>
+                <Grid xs={2}>
+                  <Paper className={classes.paper}>{gunName}</Paper>
+                </Grid>
+                <Grid xs={2}>
+                  <Paper className={classes.paper}>{gunName}</Paper>
+                </Grid>
               </Grid>
-             
+              <Grid className='achievements' item xs container direction='row'>
+                <Grid xs={2}>
+                  <Paper className={classes.paper}>{gunName}</Paper>
+                </Grid>                
+                <Grid xs={2}>
+                  <Paper className={classes.paper}>{gunName}</Paper>
+                </Grid>                
+                <Grid xs={2}>
+                  <Paper className={classes.paper}>{gunName}</Paper>
+                </Grid>                
+                <Grid xs={2}>
+                  <Paper className={classes.paper}>{gunName}</Paper>
+                </Grid>                
               </Grid>
               <Grid item>
-                <Typography variant="body2" style={{ cursor: 'pointer' }}>
-                  Remove
-                </Typography>
+              <Button variant="contained" color="secondary">Remove</Button>
               </Grid>
             </Grid>
-            <Grid item>
-              <Typography variant="subtitle1">$19.00</Typography>
+          </Grid>
+          <Grid item xs sm container style={{maxWidth: '34%'}}>
+            <Grid item xs container direction='column' spacing={2}>
+              <Grid item xs container direction='row' spacing={2}>
+                <Grid xs={2}>
+                  <Paper className={classes.paper}>{gunName}</Paper>
+                </Grid>
+                <Grid xs={2}>
+                  <Paper className={classes.paper}>{gunName}</Paper>
+                </Grid>
+                <Grid xs={2}>
+                  <Paper className={classes.paper}>{gunName}</Paper>
+                </Grid>
+                <Grid xs={2}>
+                  <Paper className={classes.paper}>{gunName}</Paper>
+                </Grid>
+                <Grid xs={2}>
+                  <Paper className={classes.paper}>{gunName}</Paper>
+                </Grid>
+                <Grid xs={2}>
+                  <Paper className={classes.paper}>{gunName}</Paper>
+                </Grid>
+              </Grid>
+              <Grid className='achievements' item xs container direction='row'>
+                <Grid xs={2}>
+                  <Paper className={classes.paper}>{gunName}</Paper>
+                </Grid>                
+                <Grid xs={2}>
+                  <Paper className={classes.paper}>{gunName}</Paper>
+                </Grid>                
+                <Grid xs={2}>
+                  <Paper className={classes.paper}>{gunName}</Paper>
+                </Grid>                
+                <Grid xs={2}>
+                  <Paper className={classes.paper}>{gunName}</Paper>
+                </Grid>                
+              </Grid>
             </Grid>
           </Grid>
         </Grid>
--- src/RouterPages/Profile.js
+++ src/RouterPages/Profile.js
@@ -1,40 +1,75 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios';
 import { useCookies } from 'react-cookie';
+import { useHistory } from 'react-router-dom';
+
+/** Material UI Imports */
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+import { makeStyles } from '@material-ui/core/styles';
+import ButtonBase from '@material-ui/core/ButtonBase';
+
+/** Local imports */
 import '../styles/profile.css'
 import ProfileNavBar from '../Profile/ProfileNavBar'
 import TrackedGuns from '../Profile/TrackedGuns.js'
 import GamerStats from '../Profile/GamerStats';
-// import { cookies } from '../hooks/useApplicationData';
 
 
-export default function Profile(props) {
-  const [cookies, setCookie] = useCookies(['name']);
+
+/** Declare Material UI classes styles */
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+    background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+    border: 0,
+    borderRadius: 3,
+    boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+    color: 'white',
+    height: 48,
+    padding: '0 30px',
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: 'center',
+    // color: theme.palette.text.secondary,
+    background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+    border: 0,
+    borderRadius: 3,
+    boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+    color: 'white',
+    textSize: '24px'
+  },
+}));
   
+let compareGamerTag = ''
+export default function Profile(props) {
+  const classes = useStyles();
+  const [cookies, setCookie, removeCookie] = useCookies(['name']);
+  const history = useHistory();
+
+  (Object.keys(cookies).length > 0 ? compareGamerTag = cookies.gamerTagInfo.gamerTag.replace('%23', '#') : compareGamerTag = '');
   const [state, setState] = useState({
     favorites: [],
+    gunStats: [],
     achievements: [],
     displayedCards: [],
     weeklyData: [],
     lifetimeData: [],
     compareGamerGuns: [],
-    shownTab: 0,
+    shownTab: 1,
   });
-  // const { cookies } = useApplicationData();
   console.log('Cookies in profile: ', cookies);
 
   useEffect(() => {
     const gamerTag = "Nickmercs%2311526";
     const gamerPlatform = "battle";
-    // const compareGamerTag = "SardarMamad%233717309";
-    // const compareGamerPlatform = "acti";
     if(Object.keys(cookies).length > 0) {
-      console.log("Something is happenng here....")
       const compareGamerTag = cookies.gamerTagInfo.gamerTag;
       const compareGamerPlatform = cookies.gamerTagInfo.gamerPlatform;
-      console.log('what am i doing here???', compareGamerTag, compareGamerPlatform)
       axios.get(`http://localhost:3030/stats/${compareGamerTag}&${compareGamerPlatform}`)
       .then((compareGamerData) => {
+        // console.log('PROFILE - Compare Gamer guns: ', compareGamerData);
         console.log('PROFILE - Compare Gamer guns: ', compareGamerData.data[2].guns);
         const compareGamerGuns = [compareGamerData.data[2].guns];
         setState(prev => ({
@@ -52,28 +87,29 @@ export default function Profile(props) {
       axios.get(`http://localhost:3030/stats/${gamerTag}&${gamerPlatform}`)
     ])
       .then(([favorites, achievements, allData]) => {
-        console.log('PROFILE - Favourites Data: ', favorites);
-  
         if (allData.data[0].weeklyData !== null) {
           const weeklyStatData = [allData.data[0].weeklyData.all.properties];
           const lifetimeStatData = [allData.data[3].lifetimeData];
+          const gunStats = [allData.data[2].guns]
           setState(prev => ({
             ...prev,
             favorites: favorites.data,
             achievements: achievements.data,
             weeklyData: weeklyStatData,
             lifetimeData: lifetimeStatData,
-          }))
-        }
-        else {
+            gunStats: gunStats,
+          }));
+        } else {
           const lifetimeStatData = [allData.data[3].lifetimeData];
+          const gunStats = [allData.data[2].guns]
           setState(prev => ({
             ...prev,
             favorites: favorites.data,
             achievements: achievements.data,
             lifetimeData: lifetimeStatData,
-          }))
-        }
+            gunStats: gunStats,
+          }));
+        };
       })
       .catch(error => {
         console.log("Error fetching Logged User Stats", error);
@@ -87,14 +123,17 @@ export default function Profile(props) {
       displayedCards: cardShown
     }));
   };
-
   
+  const removeCompare = () => {
+    removeCookie('gamerTagInfo', { path: '/' });
+    history.go(0);
+  };
+
   const categorySelected = (indexValue) => {
-    // This function sets the state for the specific shown state and category if we dont set states here the page crashes when you change from a stats to guns... because the guns component cant read the stats object and vice versa
-       setState(prev => ({
-        ...prev,
-        shownTab: indexValue
-      }))
+    setState(prev => ({
+    ...prev,
+    shownTab: indexValue
+    }))
   };
 
   const navBarsToShow = () => {
@@ -111,18 +150,63 @@ export default function Profile(props) {
     }
     else {
       if (state.favorites.length > 0) {
-        return (
-          <div className='tracked-guns'>
-          <TrackedGuns
-            key={state.favorites}
-            favorites={state.favorites}
-            displayedCards={state.displayedCards}
-            deleteStat={deleteStat}
-            achievements={state.achievements}
-            compareGamerGuns={state.compareGamerGuns}
-            />
-        </div>
-        )
+        if(Object.keys(cookies).length > 0) {
+          return (
+            <div className='tracked-guns'>
+              <div className='tracked-guns-header'>
+                <br />
+                <Grid container spacing={1}>
+                  <Grid item xs={7}>
+                    <Paper className={classes.paper}>Stats for my favorite Guns</Paper>
+                  </Grid>
+                  <Grid item xs={5}>
+                    <Paper className={classes.paper} onClick={() => {removeCompare()}}>Click here to STOP Compare with {compareGamerTag}</Paper>                    
+                  </Grid>
+                </Grid>
+                <br />
+              </div>
+              <div>
+                <TrackedGuns
+                key={state.favorites}
+                favorites={state.favorites}
+                gunStats={state.gunStats}
+                displayedCards={state.displayedCards}
+                deleteStat={deleteStat}
+                achievements={state.achievements}
+                compareGamerGuns={state.compareGamerGuns}
+                />
+              </div>
+            </div>
+          );  
+        } else {
+            return (
+              <div className='tracked-guns'>
+                <div className='tracked-guns-header'>
+                  <br />
+                  <Grid container spacing={1}>
+                    <Grid item xs={7}>
+                      <Paper className={classes.paper}>Stats for my favorite Guns</Paper>
+                    </Grid>
+                    <Grid item xs={5}>
+                      <Paper className={classes.paper}>Compare Stats with {compareGamerTag}</Paper>
+                    </Grid>
+                  </Grid>
+                  <br />
+                </div>
+                <div>
+                  <TrackedGuns
+                  key={state.favorites}
+                  favorites={state.favorites}
+                  gunStats={state.gunStats}
+                  displayedCards={state.displayedCards}
+                  deleteStat={deleteStat}
+                  achievements={state.achievements}
+                  compareGamerGuns={state.compareGamerGuns}
+                  />
+                </div>
+              </div>
+            );  
+        }
       }
       else {
         return (
